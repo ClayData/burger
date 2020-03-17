a1ec79d928b09bd4bfba896b2371b3dbc2d2b394
ClayData <claytonortiz96@gmail.com>
2020-03-17 10:32:58 -0500

    created burger object

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,34 @@
 var connection = require("../config/connection");
 
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+
+  function objToSql(ob) {
+    var arr = [];
+  
+   
+    for (var key in ob) {
+      var value = ob[key];
+     
+      if (Object.hasOwnProperty.call(ob, key)) {
+       
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+       
+        arr.push(key + "=" + value);
+      }
+    }
+    return arr.toString();
+  }
+
 let orm = {
     all: function(tableInput, cb) {
         let queryString = "SELECT * FROM " + tableInput + ";";
@@ -48,3 +77,5 @@ let orm = {
         });
     }
 }
+
+module.exports = orm;
