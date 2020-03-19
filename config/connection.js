50724ba0cbe004bda8bdbb5159b4baf285106836
ClayData <claytonortiz96@gmail.com>
2020-03-19 14:34:01 -0500

    possible heroku solution

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
+  port = process.env.PORT || 5000;
 
+if (port === 5000) {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -7,7 +9,10 @@ var connection = mysql.createConnection({
   password: "",
   database: "burgers_db"
 });
-
+}
+else{
+  process.env.DATABASE_URL;
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
